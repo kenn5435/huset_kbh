e068337669d41d3a6f994c607d7049d41809fd0d
Kenny Kennedy <kennykennedy@Kennys-MacBook-Pro-3.local>
2018-11-09 10:08:41 +0100

    New

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $('#open').on('click', function () {
+        $('#overlay').removeClass('close');
+    });
+
+    $('#close').on('click', function () {
+        $('#overlay').addClass('close');
+    });
+});
